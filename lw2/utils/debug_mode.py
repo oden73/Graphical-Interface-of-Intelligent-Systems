a1804c08ef77e459dbf2dc4f6cfe47ada288370d
oden73 <karpuk.max1@gmail.com>
2025-04-24 20:14:15 +0300

    utils implementation

--- lw2/utils/debug_mode.py
+++ lw2/utils/debug_mode.py
@@ -1,2 +1,24 @@
-class DebugMode:
-    pass
+from typing import Generator
+
+
+class StepDebugger:
+    def __init__(self, generator: Generator[dict, None, None]) -> None:
+        self.steps: list = list(generator)
+        self.index: int = 0
+
+    def current(self) -> list:
+        return self.steps[self.index]['points'] if self.index < len(self.steps) else []
+
+    def next(self) -> list:
+        if self.index < len(self.steps) - 1:
+            self.index += 1
+        return self.current()
+
+    def prev(self) -> list:
+        if self.index > 0:
+            self.index -= 1
+        return self.current()
+
+    def reset(self):
+        self.index = 0
+        return self.current()
--- lw2/utils/grid_helper.py
+++ lw2/utils/grid_helper.py
@@ -1,2 +1,18 @@
-class GridHelper:
-    pass
+from PyQt5.QtGui import QPainter, QPen, QColor
+from PyQt5.QtCore import Qt
+
+
+def draw_grid(painter: QPainter, width: int, height: int, step: int):
+    pen = QPen(QColor(220, 220, 220))
+    pen.setStyle(Qt.DotLine)
+    painter.setPen(pen)
+
+    for x in range(0, width, step):
+        painter.drawLine(x, 0, x, height)
+    for y in range(0, height, step):
+        painter.drawLine(0, y, width, y)
+
+    pen = QPen(Qt.black)
+    painter.setPen(pen)
+    painter.drawLine(0, height // 2, width, height // 2)
+    painter.drawLine(width // 2, 0, width // 2, height)
