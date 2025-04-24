13d343862be960718a09b6cbc5f7192941af9b74
oden73 <karpuk.max1@gmail.com>
2025-04-24 20:44:42 +0300

    entry point implementation

--- lw2/main.py
+++ lw2/main.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-    pass
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui_components.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
