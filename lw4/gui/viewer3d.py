cube rotation and position fix #2

lw4/gui/viewer3d.py
@@ -43,8 +43,9 @@ class Viewer3D(QWidget):
         painter.setPen((QPen(Qt.black, 2)))
 
         center: QPoint = QPoint(self.width() // 2, self.height() // 2)
+        view_matrix: np.ndarray = matrix_utils.translation_matrix(0, 0, -5)
         edges: list[tuple[np.ndarray, np.ndarray]] = self.object3d.get_transformed_edges(
-            self.projection_matrix @ self.transform.get_matrix()
+            self.projection_matrix @ view_matrix @ self.transform.get_matrix()
         )
         print(f"[DEBUG] Edges to draw: {len(edges)}")
 
@@ -72,17 +73,17 @@ class Viewer3D(QWidget):
             t = matrix_utils.translation_matrix(0, 0, -0.1)
 
         elif key == Qt.Key_Left:
-            t = matrix_utils.rotate_about_center(center, matrix_utils.rotation_matrix_y(np.radians(-10)))
+            t = matrix_utils.rotation_matrix_y(np.radians(-10))
         elif key == Qt.Key_Right:
-            t = matrix_utils.rotate_about_center(center, matrix_utils.rotation_matrix_y(np.radians(10)))
+            t = matrix_utils.rotation_matrix_y(np.radians(10))
         elif key == Qt.Key_Up:
-            t = matrix_utils.rotate_about_center(center, matrix_utils.rotation_matrix_x(np.radians(-10)))
+            t = matrix_utils.rotation_matrix_x(np.radians(-10))
         elif key == Qt.Key_Down:
-            t = matrix_utils.rotate_about_center(center, matrix_utils.rotation_matrix_x(np.radians(10)))
+            t = matrix_utils.rotation_matrix_x(np.radians(10))
         elif key == Qt.Key_C:
-            t = matrix_utils.rotate_about_center(center, matrix_utils.rotation_matrix_z(np.radians(-10)))
+            t = matrix_utils.rotation_matrix_z(np.radians(-10))
         elif key == Qt.Key_V:
-            t = matrix_utils.rotate_about_center(center, matrix_utils.rotation_matrix_z(np.radians(10)))
+            t = matrix_utils.rotation_matrix_z(np.radians(10))
 
         elif key == Qt.Key_Plus or key == Qt.Key_Equal:
             t = matrix_utils.scale_matrix(1.1, 1.1, 1.1)