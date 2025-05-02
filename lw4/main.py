f159dcee29d0420b2a519a3b284bc4a7ec7b4d35
oden73 <karpuk.max1@gmail.com>
2025-05-02 11:41:42 +0300

    main.py implementation

--- lw4/main.py
+++ lw4/main.py
@@ -1,20 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-from gui.ui.main_window import MainWindow
-from src.core.object_3d import Object3D
+from gui.main_window import MainWindow
 
 
-def main():
-    app = QApplication(sys.argv)
-
-    object_file = 'assets/object.txt'
-    object = Object3D.create_from_file(object_file)
-
-    window = MainWindow(object)
-    window.resize(1000, 700)
+def main() -> None:
+    app: QApplication = QApplication(sys.argv)
+    window: MainWindow = MainWindow()
     window.show()
-
     sys.exit(app.exec_())
 
 
