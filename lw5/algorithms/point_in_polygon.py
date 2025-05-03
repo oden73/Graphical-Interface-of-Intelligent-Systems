9db8a3ca92943dc88a92ee94316cec5dfd280a8e
oden73 <karpuk.max1@gmail.com>
2025-05-03 19:43:08 +0300

    point inside polygon check fix #2

--- lw5/algorithms/point_in_polygon.py
+++ lw5/algorithms/point_in_polygon.py
@@ -6,28 +6,17 @@ from models.polygon import Polygon
 
 
 def is_point_in_polygon(point: Point, polygon: Polygon) -> bool:
-    def angle_between(p1: Point, p2: Point, p: Point) -> float:
-        dx1: float = p1.x - p.x
-        dy1: float = p1.y - p.y
-        dx2: float = p2.x - p.x
-        dy2: float = p2.y - p.y
+    points: List[Point] = polygon.points
+    num_points: int = len(points)
 
-        angle1: float = math.atan2(dy1, dx1)
-        angle2: float = math.atan2(dy2, dx2)
-        angle: float = angle2 - angle1
+    intersections: int = 0
 
-        if angle <= -math.pi:
-            angle += 2 * math.pi
-        elif angle > math.pi:
-            angle -= 2 * math.pi
+    for i in range(num_points):
+        p1: Point = points[i]
+        p2: Point = points[(i + 1) % num_points]
+        
+        if ((p1.y > point.y) != (p2.y > point.y)) and \
+                (point.x < (p2.x - p1.x) * (point.y - p1.y) / (p2.y - p1.y) + p1.x):
+            intersections += 1
 
-        return angle
-
-    total_angle: float = 0.
-    n: int = len(polygon.points)
-    for i in range(n):
-        p1: Point = polygon.points[i]
-        p2: Point = polygon.points[(i + 1) % n]
-        total_angle += angle_between(p1, p2, point)
-
-    return abs(abs(total_angle) - 2 * math.pi) < 1e-4
+    return intersections % 2 == 1
